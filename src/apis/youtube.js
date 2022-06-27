24c17ef50bfa2a0d81417740cbb5149eaeb33a9a
waiting_su <waiting_su@gss.com.tw>
2022-06-27 21:59:32 +0800

    調整key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY='AIzaSyDSgoDuNS6IysvBD8TRC6LV7C-mid0AS68';
+const KEY=process.env.REACT_APP_YOUTUBE_KEY;
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
